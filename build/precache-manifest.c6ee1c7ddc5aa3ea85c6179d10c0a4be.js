31bf0e0e920a65d60087838c8da3c1f6567e2c52
Kemal Akoglu <kemal.akoglu@doco.com.tr>
2020-04-08 13:43:10 +0300

    Deployment Options were set up

---
+++ build/precache-manifest.c6ee1c7ddc5aa3ea85c6179d10c0a4be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3280cb8a85befd2e3bcdaa10d434322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d794caac3c909524669a",
+    "url": "/static/js/2.979ef0a8.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/static/js/2.979ef0a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c89819bb6fc010b8e9",
+    "url": "/static/js/main.9644565b.chunk.js"
+  },
+  {
+    "revision": "eafb343973d9ec89fe65",
+    "url": "/static/js/runtime-main.bb72b785.js"
+  }
+]);
